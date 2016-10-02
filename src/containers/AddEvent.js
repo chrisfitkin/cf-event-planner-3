af8a951ee55570afa00d174b7260422d3cf8d415
chrisfitkin <cfitkin@gmail.com>
2016-10-01 17:09:24 -0700

    Additional Form Elements

--- src/containers/AddEvent.js
+++ src/containers/AddEvent.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux'
 import { addEvent } from '../actions'
 
 import TextField from 'material-ui/TextField'
-// import RaisedButton from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import moment from 'moment';
-// import DatePicker from 'react-toolbox/lib/date_picker';
-// import DatePicker from 'material-ui/DatePicker';
+import DatePicker from 'material-ui/DatePicker'
+import TimePicker from 'material-ui/TimePicker'
+import AutoComplete from 'material-ui/AutoComplete'
+import MenuItem from 'material-ui/MenuItem';
+import AddressAutoComplete from '../components/AddressAutoComplete'
 
 // Check this out for submit handling
 // https://github.com/chrisfitkin/react-redux-blog/blob/master/public/src/components/PostsForm.js
@@ -14,6 +17,7 @@ let AddEvent = ({ dispatch }) => {
   let title, host, eventType, startDate, startTime, endDate, endTime, guestList, location, message
   let today = new Date();
   let todayFormatted = moment(today).format('MM/DD/YYYY');
+  const eventTypeOptions = ["Birthday Party", "Conference", "Wedding", "Dinner", "Meet Up", "Work Meeting", "Drinks", "Baseball Game"]
   return (
     <div>
       <form onSubmit={e => {
@@ -34,10 +38,66 @@ let AddEvent = ({ dispatch }) => {
         autoFocus
         required
         ref={node => { title = node}}
-      /><br/><br/>
-      <button className='btn btn-default' type="submit">
+      /><br/>
+      <AutoComplete
+        floatingLabelText="Type of event"
+        hintText="Birthday, Meet Up, etc..."
+        filter={AutoComplete.fuzzyFilter}
+        openOnFocus={true}
+        dataSource={eventTypeOptions}
+        required
+        ref={node => { eventType = node }}
+      /><br/>
+      <AddressAutoComplete
+        floatingLabelText="Where is it"
+        hintText="123 Main Street, Los Angeles, CA"
+        required
+        ref={node => { location = node }}
+      /><br/>
+      <TextField
+        hintText="John Smith or Acme, Co."
+        floatingLabelText="Who is hosting"
+        autoComplete="name"
+        required
+        ref={node => { host = node}}
+      /><br/>
+      <DatePicker
+        autoOk
+        floatingLabelText="What day"
+        type="text"
+        name='startDate'
+        ref={ref => { startDate = ref }}
+        hintText={todayFormatted}
+      />
+      <TimePicker
+        hintText="12hr Format"
+        autoOk
+        floatingLabelText="What time"
+        name="startTime"
+        ref={ref => { startTime = ref }}
+        format="ampm"
+      />
+      <DatePicker
+        autoOk
+        floatingLabelText="Ending day"
+        type="text"
+        name='endDate'
+        ref={ref => { endDate = ref }}
+        hintText={todayFormatted}
+      />
+      <TimePicker
+        hintText="12hr Format"
+        autoOk
+        name="endTime"
+        ref={ref => { endTime = ref }}
+        floatingLabelText="Ending time"
+        format="ampm"
+      />
+      <br/>
+      <br/>
+      <RaisedButton className='btn btn-default' type="submit">
         Add Event
-      </button>
+      </RaisedButton>
       </form>
     </div>
   )
