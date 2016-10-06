e45014ecb314b9df5a445082e9b556fd6c4c7087
chrisfitkin <cfitkin@gmail.com>
2016-10-06 00:56:00 -0700

    inline styling, registration form is complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,23 @@ import React from 'react'
 import EventListPage from './EventListPage'
 import Header from './Header'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// style={{fontFamily: this.context.muiTheme.fontFamily}}
+
+const muiTheme = getMuiTheme({
+  fontFamily: 'Roboto, sans-serif',
+  // palette: {
+  //   textColor: cyan500,
+  // },
+  // appBar: {
+  //   height: 50,
+  // },
+});
+
 
 const App = ({children}) => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <div>
         <Header />
         <div style={{ marginTop: '1.5em' }}>{children}</div>
--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,31 +1,50 @@
-import React, { Component } from 'react'
-// const reducer = combineReducers({
-//   form: reduxFormReducer // mounted under "form"
-// })
+import React, { Component, PropTypes } from 'react'
+import Snackbar from 'material-ui/Snackbar'
+import RegisterForm from './RegisterForm'
 
-const showResults = values => {
-  console.log(values)
-  new Promise(resolve => {
-    setTimeout(() => {  // simulate server latency
-      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
-      // todo, load successful snackbar(?) or modal alert message
-      resolve()
-    }, 500)
-  })
+
+
+class RegisterPage extends Component {
+
+ constructor(props){
+    super(props)
+    this.state = {
+      open: false,
+    };
+ }
+
+  handleSubmit = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  // const RegisterForm = require('./RegisterForm').default
+
+  render() {
+    return (
+      <div>
+        <h2 style={{fontFamily: this.context.muiTheme.fontFamily}}>Form</h2>
+        <RegisterForm onSubmit={this.handleSubmit}/>
+        <Snackbar
+          open={this.state.open}
+          message="Registration successful!"
+          autoHideDuration={4000}
+          onRequestClose={this.handleRequestClose}
+        />
+      </div>
+    )
+  }
 }
 
-const RegisterPage = (props) => {
+RegisterPage.contextTypes = {
+  muiTheme: PropTypes.object.isRequired,
+};
 
-  const RegisterForm = require('./RegisterForm').default
-  return(
-        <div>
-
-          <h2>Form</h2>
-
-          <RegisterForm onSubmit={showResults}/>
-
-        </div>
-  )
-}
 
 export default RegisterPage
